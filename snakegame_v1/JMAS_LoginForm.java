cd50ef0475ae190406a0dd2bc50db6a00a4ab129
jsilva-scu <j.maia.alves.da.silva.10@student.scu.edu.au>
2021-04-22 14:48:39 +0800

    Added initials JMAS_ to PlayerList import and instantiation.

--- snakegame_v1/JMAS_LoginForm.java
+++ snakegame_v1/JMAS_LoginForm.java
@@ -25,7 +25,7 @@ public class JMAS_LoginForm extends JFrame implements ActionListener {
     private JTextField textUsername;
     private JPasswordField fieldPassword;
     private JButton buttonLogin;
-    private PlayerList playerList;
+    private JMAS_PlayerList playerList;
  
     public JMAS_LoginForm() {
         super("Login Form");
@@ -76,7 +76,7 @@ public class JMAS_LoginForm extends JFrame implements ActionListener {
         buttonLogin.addActionListener(this);
         
         //instantiate the playerList
-        playerList = new PlayerList();
+        playerList = new JMAS_PlayerList();
         try {
             readPlayerFromFile("players.txt");
         } catch (FileNotFoundException e) {
